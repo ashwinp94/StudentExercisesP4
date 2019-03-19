2b06ec1495daf6a1eb50b4901d2bfef7ec1fd875
Ashwin Prakash <ashp94@gmail.com>
2019-03-19 14:10:43 -0500

    Add project files.

---
+++ studentExercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace studentExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
